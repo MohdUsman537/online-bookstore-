ca51b0da5291a47c524ffe5ac44dfa9c3d27f6e9
MohdUsman537 <164644491+MohdUsman537@users.noreply.github.com>
2025-03-27 02:16:50 +0530

    Making the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { RiBarChartHorizontalLine } from "react-icons/ri";
+import { IoSearchCircle } from "react-icons/io5";
+
 
 const Navbar = () => {
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
     <nav className="flex justify-between items-center">
          {/* leftside*/}
-         <div>
-          <Link to="/">Logo</Link>
+         <div className="flex items-center md-gap-16 gap-4">
+          <Link to="/">
+             <RiBarChartHorizontalLine className="size-6"/>
+          </Link>
+
+          {/*search input*/}
+          <div className="relative sm:w-72 w-40 space-x-2">
+          <IoSearchCircle className="absolute inline-block left-3 inset-y-2"/>
+          <input type="text" placeholder='Search here' className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"/>
+          </div>
          </div>
          {/*rightside*/}
          <div>
